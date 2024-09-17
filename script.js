9567ab2d3619036a0c47d04e85adfd7b1ead5149
RyanPi <ryan.pikulski@gmail.com>
2024-09-17 10:05:33 -0700

    moved script to separate file 3.0.

---
+++ script.js
@@ -0,0 +1,29 @@
+function retrieveEnvelope(callback) {
+    let env = undefined;
+    try {
+      env =
+   decodeURIComponent(document.cookie.match('(^|;) *_lr_env=([^;]*)')[2]);
+        env = {
+            'envelope': JSON.parse(atob(env)).envelope
+        };
+  env = JSON.stringify(env);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      if(callback){
+        callback(env);
+      } else {
+        return env;
+      }
+    }
+  }
+
+const button = document.querySelector(".submit-button");
+button.addEventListener("click", ()=>{
+    console.log("hello");
+    const p = document.createElement("p");
+    const div = document.querySelector(".form-handler");
+    env = retrieveEnvelope()
+    p.innerHTML = env;
+    div.append(p);
+},false);
