6bd247a1f8792881c229c77de6b58166844abeda
RyanPi <ryan.pikulski@gmail.com>
2024-09-18 11:17:03 -0700

    added code for decoded env testing 3.0.

--- script.js
+++ script.js
@@ -20,15 +20,19 @@
     env = decodeURIComponent(document.cookie.match('(^|;) *_lr_env=([^;]*)')[2]);
     env_object = {'envelope': JSON.parse(atob(env)).envelope};
     console.log(`this is env_object value:${env_object.envelope}`);
-    string_env = JSON.stringify(env_object);
-    console.log(`this is env_object value:${string_env}`);
+    // string_env = JSON.stringify(env_object);
+    // console.log(`this is env_object value:${string_env}`);
 
-    const p = document.querySelector(".envelope");
+    const pe = document.querySelector(".encoded-envelope");
+    const pd = document.querySelector(".decoded-envelope");
     const div = document.querySelector(".form-handler");
-    if(p.innerHTML===""){
-      p.innerHTML = env;
+    if(pe.innerHTML===""){
+      pe.innerHTML = env;
+      pd.innerHTML = env_object.envelope;
+
     } else {
-      p.innerHTML = env;
+      pe.innerHTML = env;
+      pd.innerHTML = env_object.envelope;
     }
   }
 
