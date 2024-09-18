f47ff9172e87270e1ad3f64bdab0d5f77b7ab89f
RyanPi <ryan.pikulski@gmail.com>
2024-09-17 19:19:43 -0700

    added timout.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function retrieveEnvelope(callback) {
+function retrieveEnvelope(){
     let env = undefined;
     try {
       env = decodeURIComponent(document.cookie.match('(^|;) *_lr_env=([^;]*)')[2]);
@@ -10,12 +10,16 @@ function retrieveEnvelope(callback) {
         return env;
     }
   }
+    
+
 
 const button = document.querySelector(".submit-button");
 button.addEventListener("click", ()=>{
+    console.log(1);
+    const env = setTimeout(retrieveEnvelope, 2000);
+    console.log(2);
     const p = document.createElement("p");
     const div = document.querySelector(".form-handler");
-    env = retrieveEnvelope();
     p.innerHTML = env;
     div.append(p);
 },false);
