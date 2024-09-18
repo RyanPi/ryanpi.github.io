2fc9be20f93ff15fd009f7d8b44d875418a3faa6
RyanPi <ryan.pikulski@gmail.com>
2024-09-17 19:46:18 -0700

    added timout.

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 
-function retrieveEnvelope(){
-    let env = undefined;
-    try {
-      env = decodeURIComponent(document.cookie.match('(^|;) *_lr_env=([^;]*)')[2]);
-      // env = {'envelope': JSON.parse(atob(env)).envelope};
-      // env = JSON.stringify(env);
-    } catch (e) {
-      console.log(e);
-    } finally {
-        return env;
-    }
+// function retrieveEnvelope(){
+//     let env = undefined;
+//     try {
+//       env = decodeURIComponent(document.cookie.match('(^|;) *_lr_env=([^;]*)')[2]);
+//       // env = {'envelope': JSON.parse(atob(env)).envelope};
+//       // env = JSON.stringify(env);
+//     } catch (e) {
+//       console.log(e);
+//     } finally {
+//         return env;
+//     }
+//   }
+
+  //Get Envelope Value
+  function getEnvelopeToPage(){
+    env = decodeURIComponent(document.cookie.match('(^|;) *_lr_env=([^;]*)')[2]);
+    const p = document.createElement("p");
+    const div = document.querySelector(".form-handler");
+    p.innerHTML = env;
+    div.append(p)
   }
+
     
 
 
 const button = document.querySelector(".submit-button");
-button.addEventListener("click", ()=>{
-    const p = document.createElement("p");
-    const div = document.querySelector(".form-handler");
-
-    setTimeout(retrieveEnvelope, 3000);
-
- 
-    p.innerHTML = env;
-    div.append(p);
-},false);
+button.addEventListener("click", ()=>{setTimeout(getEnvelopeToPage,3000)},false);
